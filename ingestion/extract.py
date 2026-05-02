97af6be71553515da9ba456755738ef4f83ec4ab
Jahanzaib Irfan <jahanzaibqutbi1@gmail.com>
2026-05-02 16:45:45 +0500

    Initial Commit

---
+++ ingestion/extract.py
@@ -0,0 +1,46 @@
+import requests
+import pandas as pd 
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+BASE_URL = "https://www.alphavantage.co/query"
+
+
+def fetch_daily_stock(symbol : str) -> pd.DataFrame:
+    params = {
+        "function" : "TIME_SERIES_DAILY",
+        "symbol": symbol,
+        "apikey": API_KEY,
+        "outputsize":"compact"   
+    }
+
+    response = requests.get(BASE_URL , params= params)
+    response.raise_for_status()
+
+    data = response.json()
+
+    time_series = data.get("Time Series (Daily)", {})
+
+    if not time_series:
+        raise ValueError(f"No data returned for {symbol}. Check your API key or symbol.")
+        
+    df = pd.DataFrame.from_dict(time_series , orient='index')
+    df.index.name = 'date'
+    df.reset_index(inplace = True)
+
+    df.columns = ["date" , "open" , "high" , "low" , "close" , "volume"]
+    df['symbol'] = symbol
+    df['date'] = pd.to_datetime(df['date'])
+
+    return df 
+
+if __name__ == "__main__":
+    df = fetch_daily_stock("AAPL")
+    print(df.head())
+    print(f"\nShape: {df.head}")
+
+
+
---
+++ tests/test_extract.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+from unittest.mock import patch, MagicMock
+from ingestion.extract import fetch_daily_stock
+
+
+MOCK_RESPONSE = {
+    "Time Series (Daily)": {
+        "2024-01-01": {
+            "1. open": "150.0", "2. high": "155.0",
+            "3. low": "149.0", "4. close": "153.0", "5. volume": "1000000"
+        }
+    }
+}
+
+@patch("ingestion.extract.requests.get")
+def test_fetch_returns_dataframe(mock_get):
+    mock_get = return_value = MagicMock(
+        status_code = 200,
+        json = lambda: MOCK_RESPONSE
+    )
+
+    df = fetch_daily_stock("AAPL")
+    assert isinstance(df , pd.DataFrame)
+    assert "close" in df.columns
+    assert df['symbol'].iloc[0] == "AAPL"
